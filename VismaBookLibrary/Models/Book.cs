98cb5122565fbef09d9be067330ada1839502484
Mariuš <m.voitkun@gmail.com>
2021-10-07 15:08:07 +0300

    Initial commit

---
+++ VismaBookLibrary/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VismaBookLibrary.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public string Category { get; set; }
+
+        public string Language { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        public string ISBN { get; set; }
+
+        public bool IsLent { get; set; }
+    }
+}
---
+++ VismaBookLibrary/Models/Lending.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VismaBookLibrary.Models
+{
+    public class Lending
+    {
+        public int BookId { get; set; }
+
+        public int ClientId { get; set; }
+
+        public DateTime LendingDate { get; set; }
+
+        public DateTime LentUntil { get; set; }
+
+        public DateTime ReturnDate { get; set; }
+    }
+}
